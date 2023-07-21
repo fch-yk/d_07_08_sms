d713d749e8b2a3a27ab596689221fe6b337578ba
fch-yk <101698505+fch-yk@users.noreply.github.com>
2023-07-21 22:19:20 +0300

    Add SMS status check

--- smsc_api.py
+++ smsc_api.py
@@ -5,6 +5,37 @@ import warnings
 from dotenv import load_dotenv
 
 
+async def send_sms(login, password, message, phones, valid):
+    payload = {
+        'login': login,
+        'psw': password,
+        'mes': message,
+        'phones': phones,
+        'valid': valid,
+        'cost': '3',
+        'fmt': '3',
+    }
+    url = 'https://smsc.ru/sys/send.php'
+    response = await asks.get(url, params=payload)
+    response.raise_for_status()
+    return response.json()
+
+
+async def get_sms_status(login, password, phones, sms_id, fmt=3, all=2):
+    payload = {
+        'login': login,
+        'psw': password,
+        'phone': phones,
+        'id': sms_id,
+        'fmt': fmt,
+        'all': all,
+    }
+    url = 'https://smsc.ru/sys/status.php'
+    response = await asks.get(url, params=payload)
+    response.raise_for_status()
+    return response.json()
+
+
 @click.command()
 @click.option(
     '--login',
@@ -37,16 +68,15 @@ from dotenv import load_dotenv
     help='message time to live (in hours)',
 )
 async def main(login, password, message, phones, valid):
-    payload = {
-        'login': login,
-        'psw': password,
-        'mes': message,
-        'phones': phones,
-        'valid': valid,
-    }
-    url = 'https://smsc.ru/sys/send.php'
-    response = await asks.get(url, params=payload)
-    response.raise_for_status()
+    send_response = await send_sms(login, password, message, phones, valid)
+    # send_response = {'id': 411}
+    status_response = await get_sms_status(
+        login,
+        password,
+        phones,
+        sms_id=send_response['id']
+    )
+    print(status_response)
 
 if __name__ == '__main__':
     load_dotenv()
