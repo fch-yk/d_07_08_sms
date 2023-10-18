8b84e2be148f57cfe6e58ed3aefd0e42fdfd8d86
fch-yk <yefim_korshever@mail.ru>
2023-10-18 23:06:00 +0300

    Save SMS mailings to Redis db

--- quart_server.py
+++ quart_server.py
@@ -3,17 +3,22 @@ import logging
 import os
 from unittest.mock import patch
 
+import aioredis
 import trio
+import trio_asyncio
 from dotenv import load_dotenv
+from hypercorn.config import Config as HyperConfig
+from hypercorn.trio import serve
 from pydantic import BaseModel, ValidationError, constr
 from quart import render_template, request, websocket
 from quart_trio import QuartTrio
+from trio_asyncio import aio_as_trio
 
+from db import Database
 from smsc_api import RequestSMSC
 
 app = QuartTrio(__name__)
 app.config.from_prefixed_env(prefix='SMSC')
-print(dir(app.config))
 smsc_connection = RequestSMSC(
     login=os.getenv('SMSC_LOGIN', ''),
     password=os.getenv('SMSC_PSW', '')
@@ -33,6 +38,19 @@ class SMSCSettings(BaseModel):
     valid: int
 
 
+@app.before_serving
+async def create_db_connection():
+    app.redis = aioredis.from_url(
+        os.getenv('SMSC_REDIS_URL'), decode_responses=True
+    )
+    app.db = Database(app.redis)
+
+
+@app.after_serving
+async def close_db_connection():
+    await app.redis.close()
+
+
 @app.route("/")
 async def hello():
     return await render_template("index.html")
@@ -47,10 +65,16 @@ async def send_sms():
     except ValidationError as e:
         return {"errorMessage": str(e.errors())}, 400
 
+    phones = os.getenv('SMSC_PHONES', '')
     with patch.object(
         RequestSMSC,
         'send',
-        return_value={'id': 104062995, 'cnt': 1}
+        return_value={
+            'id': 104062998,
+            'cnt': 1,
+            'cost': '4.18',
+            'balance': '770.59'
+        }
     ):
         request_smsc = RequestSMSC(
             login=os.getenv('SMSC_LOGIN', ''),
@@ -58,7 +82,7 @@ async def send_sms():
         )
         sending_response = await request_smsc.send(
             input_form.text,
-            os.getenv('SMSC_PHONES', ''),
+            phones,
             os.getenv('SMSC_VALID', '')
         )
 
@@ -68,10 +92,21 @@ async def send_sms():
     # )
     # sending_response = await request_smsc.send(
     #     input_form.text,
-    #     os.getenv('SMSC_PHONES', ''),
+    #     phones,
     #     os.getenv('SMSC_VALID', '')
     # )
     logger.info('sending_response: %s', sending_response)
+
+    await aio_as_trio(
+        app.db.add_sms_mailing(
+            sending_response['id'], phones.split(','), input_form.text
+        )
+    )
+    sms_ids = await aio_as_trio(app.db.list_sms_mailings())
+    logger.info('There are SMS IDs in the redis DB: %s', sms_ids)
+    sms_mailings = await aio_as_trio(app.db.get_sms_mailings(*sms_ids))
+    logger.info(sms_mailings)
+
     return sending_response
 
 
@@ -121,8 +156,15 @@ async def ws():
             delivered_02 = 0
 
 
+async def run_server():
+    async with trio_asyncio.open_loop():
+        config = HyperConfig()
+        config.bind = ["127.0.0.1:5000"]
+        config.use_reloader = True
+        await serve(app, config)
+
 if __name__ == "__main__":
     logging.basicConfig()
     logger.setLevel(logging.INFO)
     load_dotenv()
-    app.run()
+    trio.run(run_server)
